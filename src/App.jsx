eb2fba3c2dee182bf02eb30a4bed5ae28a408d3f
LJBL22 <yourgitemail@example.com>
2023-03-22 10:26:59 +0800

    feat: add login/signup/admin route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import {
+  SignupPage,
+  LoginPage,
+  AdminLoginPage,
+  AdminUsersPage,
+  AdminTweetsPage,
+} from "pages";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello ALPHitter</h1>
+      <BrowserRouter>
+        <h1>Hello ALPHitter</h1>
+        <Routes>
+          {/* 這個會失效 */}
+          {/* <Route path="/" element={<App />}> */}
+          <Route index element={<LoginPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignupPage />} />
+          <Route path="admin" element={<AdminLoginPage />} />
+          <Route path="admin/users" element={<AdminUsersPage />} />
+          <Route path="admin/tweets" element={<AdminTweetsPage />} />
+          {/* 以下暫時無法 */}
+          {/* <Route path="admin" element={<AdminLoginPage />}>
+            <Route path="users" element={<AdminUsersPage />} />
+            <Route path="tweets" element={<AdminTweetsPage />} />
+            <Route path="*" element={<AdminUsersPage />} />
+          </Route> */}
+          {/* </Route> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import LoginPage from './LoginPage';
+import SignupPage from './SignupPage';
+import AdminLoginPage from './AdminLoginPage';
+import AdminTweetsPage from './AdminTweetsPage';
+import AdminUsersPage from './AdminUsersPage';
+
+export { LoginPage, SignupPage, AdminLoginPage, AdminTweetsPage, AdminUsersPage };
