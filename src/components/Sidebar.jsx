f4a3f2ee75c1233af75302cd4edd0f06c1e49bd8
LJBL22 <yourgitemail@example.com>
2023-03-23 16:16:14 +0800

    feat: add Container, Icon, Admin route outlet

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,21 @@
+import { IconHomeOut, IconLogo } from "assets/icons";
+
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <>
+      <nav className="sidebar">
+        <div>
+          <IconLogo />
+        </div>
+        <div class="page-link">
+          <IconHomeOut />
+          Men
+        </div>
+        <div class="page-link">Men</div>
+        <div class="page-link">Men</div>
+      </nav>
+    </>
+  );
 }
 
 export default Sidebar;
