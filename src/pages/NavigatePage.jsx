fc3b2f0daa0fa7d393aca5428b68fe37c5e1be3a
LJBL22 <yourgitemail@example.com>
2023-03-26 22:23:21 +0800

    feat: nav page

--- src/pages/NavigatePage.jsx
+++ src/pages/NavigatePage.jsx
@@ -1,17 +1,17 @@
-// import { useAuth } from "contexts/AuthContext";
-// import { useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useAuth } from "contexts/AuthContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-// const NavigatePage = () => {
-//   const { isAuthenticated } = useAuth();
-//   const navigate = useNavigate();
-//   useEffect(() => {
-//     if (isAuthenticated) {
-//       navigate("/tweets");
-//     } else {
-//       navigate("/login");
-//     }
-//   }, [isAuthenticated, navigate]);
-// };
+const NavigatePage = () => {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/tweets");
+    } else {
+      navigate("/login");
+    }
+  }, [isAuthenticated, navigate]);
+};
 
-// export default NavigatePage;
+export default NavigatePage;
