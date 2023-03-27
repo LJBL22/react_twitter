3157adb83c96f9c04104a4936dfe0b0a67c883d1
LJBL22 <yourgitemail@example.com>
2023-03-27 17:58:30 +0800

    add: common styled Header & ThemeButton

---
+++ src/components/common/common.styled.js
@@ -0,0 +1,32 @@
+import styled from "styled-components"
+
+const StyledHeader = styled.header`
+  font-weight: 700;
+  padding: 1.5rem;
+  font-size: 1.5rem;
+  border-bottom: var(--color-gray-border) 1px solid;
+  `
+const StyledButton = styled.button`
+  border: none;
+  color: white;
+  font-weight: 400;
+  background-color: var(--color-theme);
+  padding: ${({ padding }) => padding || '8px 0'};
+  border-radius: ${({ borderRadius }) => borderRadius || '50px'};
+  height: ${({ hight }) => hight || '46px'};
+  width: ${({ width }) => width || '7rem'};
+  margin: ${({ margin }) => margin || 0};
+  /* min-width: 178px; */
+  /* height: 2.875rem; */
+  /* width: 100%; */
+  transition: background-color .1s ease-in-out;
+  &:hover {
+    cursor: pointer;
+    background-color: var(--color-primary);
+  }
+`;
+export {
+  StyledHeader as Header,
+  StyledButton as ThemeButton,
+}
+
