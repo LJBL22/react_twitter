063c6dcc647e2e3eddce54590d1ac58107e18830
LJBL22 <yourgitemail@example.com>
2023-03-29 12:33:33 +0800

    refactor: modified routes, navbar function works

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,17 @@ import AdminLayout from 'components/layouts/AdminLayout';
 import TweetLayout from 'components/layouts/TweetLayout';
 import { AuthProvider } from 'contexts/AuthContext';
 import { TweetsProvider } from 'contexts/TweetContext';
+import FollowList from 'pages/FollowList';
+import UserTweets from 'pages/UserTweets';
 // import { FollowshipProvider } from 'contexts/FollowshipContext';
 
 function App() {
   return (
     <div className='App'>
-      <GlobalStyle />
       <BrowserRouter>
         <AuthProvider>
           <TweetsProvider>
+            <GlobalStyle />
             <Routes>
               <Route path='*' element={<NavigatePage />} />
               <Route path='login' element={<LoginPage />} />
@@ -35,28 +37,32 @@ function App() {
               <Route path='admin'>
                 <Route index element={<AdminLoginPage />} />
                 <Route element={<AdminLayout />}>
-                  <Route path='users' element={<AdminUsersPage />} />
                   <Route path='tweets' element={<AdminTweetsPage />} />
+                  <Route path='users' element={<AdminUsersPage />} />
                 </Route>
               </Route>
-              {/* <FollowshipProvider> */}
               <Route element={<TweetLayout />}>
-                <Route path='/tweets' element={<HomePage />} />
-                <Route path='/tweets/:tweetId' element={<ReplyList />} />
-              </Route>
-              <Route path='users'>
-                <Route element={<TweetLayout />}>
-                  <Route index element={<UsersPage />} />
-                  <Route path='setting' element={<UserSetting />} />
-                  <Route path=':userId/tweets' element={<UsersPage />} />
-                  <Route path=':userId/likes' element={<UserLikes />} />
-                  <Route
-                    path=':userId/replied_tweets'
-                    element={<UserReplied />}
-                  />
+                <Route path='tweets'>
+                  <Route index element={<HomePage />} />
+                  <Route path=':tweetId' element={<ReplyList />} />
                 </Route>
+                <Route element={<UsersPage />}>
+                  <Route path='users/:tweetId'>
+                    <Route path='tweets' element={<UserTweets />} />
+                    <Route path='replies' element={<UserReplied />} />
+                    <Route path='likes' element={<UserLikes />} />
+                    <Route
+                      path='followers'
+                      element={<FollowList type='followers' />}
+                    />
+                    <Route
+                      path='followings'
+                      element={<FollowList type='followings' />}
+                    />
+                  </Route>
+                </Route>
+                <Route path='setting' element={<UserSetting />} />
               </Route>
-              {/* </FollowshipProvider> */}
             </Routes>
           </TweetsProvider>
         </AuthProvider>
