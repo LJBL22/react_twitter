a2fb337ffeb548910addce2a1117546974b8e09d
LJBL22 <yourgitemail@example.com>
2023-03-30 10:38:03 +0800

    page: first try but need tweetCard to confirm

--- src/pages/Admin/AdminTweetsPage.jsx
+++ src/pages/Admin/AdminTweetsPage.jsx
@@ -2,16 +2,71 @@ import { Header } from 'components/common/common.styled';
 // import TweetCollection from 'components/TweetCollection';
 import { ScrollBar } from 'pages/HomePage';
 // import { adminGetTweets, deleteTweet } from 'api/admin';
+// import TweetCard from 'components/TweetCard';
+// import { useTweets } from 'contexts/TweetContext';
+// import { useNavigate, useOutletContext } from 'react-router-dom';
+// import React, { useEffect, useState } from 'react';
 
-import React from 'react';
+export default function AdminTweetsPage() {
+  // const { tweets, setTweets } = useOutletContext();
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   const token = localStorage.getItem('adminToken');
+  //   if (!token) {
+  //     navigate('/admin');
+  //   }
+  // }, [navigate]);
 
-const AdminTweetsPage = ({ tweets }) => {
+  // // 拿到所有推文的API
+  // useEffect(() => {
+  //   const getTweetsAsync = async () => {
+  //     try {
+  //       const tweets = await adminGetTweets();
+  //       setTweets(
+  //         tweets.map((tweet) => {
+  //           return {
+  //             ...tweet,
+  //           };
+  //         })
+  //       );
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+  //   getTweetsAsync();
+  // }, []);
   return (
     <>
       <Header>推文清單</Header>
       <ScrollBar>{/* <TweetCollection tweets={tweets} /> */}</ScrollBar>
     </>
   );
-};
+}
 
-export default AdminTweetsPage;
+// 這裡的 tweets 是什麼
+// 然後要 handle 什麼
+// 是要引入還是自己寫一個？
+// function TweetCollection({ tweets }) {
+//   const { handleGetTweet } = useTweets();
+//   const navigate = useNavigate();
+
+//   const handleCardClick = (id) => {
+//     handleGetTweet(id);
+//     navigate(`/tweets/${id}`);
+//   };
+//   return (
+//     <div>
+//       {tweets.map((card) => {
+//         return (
+//           <TweetCard
+//             divWidth='40.0625rem'
+//             divHeight='auto'
+//             key={card.id}
+//             card={card}
+//             onClick={() => handleCardClick(card.id)}
+//           />
+//         );
+//       })}
+//     </div>
+//   );
+// }
